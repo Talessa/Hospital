11ab5ab508bad22f00e755c13f82edf067e0ea3f
Talessa <mjunoygomez@gmail.com>
2019-02-27 11:59:43 +0100

    Initial commit

---
+++ src/sample/Controllers/CsvController.java
@@ -0,0 +1,65 @@
+package sample.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import sample.Modelos.Hospital;
+import sample.Modelos.Paciente;
+
+import java.io.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvController {
+
+    @FXML
+    Button cargar , buscar;
+
+    @FXML
+    Label msn;
+
+    @FXML
+    TextField ruta;
+
+    File file = null;
+
+    boolean exito=false;
+
+    List<Paciente> pacientes = new ArrayList<>();
+    @FXML
+    public void initialize() {
+
+    }
+
+    @FXML public void  clicBuscar(ActionEvent event){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Seleccione CSV");
+
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("CSV","*.csv")
+        );
+
+        file = fileChooser.showOpenDialog(null);
+        ruta.setText(file.getAbsolutePath());
+    }
+
+    @FXML public void  clicCargar(ActionEvent event){
+
+        msn.setDisable(false);
+        msn.setOpacity(1);
+        pacientes = new DatosContoller().cargarcsv(file.getAbsolutePath());
+        if (pacientes.isEmpty()){
+            msn.setText("Ocurio un error intentelo mas tarde");
+        }else {
+            msn.setText("Datos introducidos correctamente");
+        }
+
+
+    }
+
+}
